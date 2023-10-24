c9231314b528e5615e4ae1f521f9b64053da8486
SunPodder <73415680+SunPodder@users.noreply.github.com>
2023-10-24 12:19:14 +0600

    add topbar

---
+++ src/lib/components/Topbar.svelte
@@ -0,0 +1,15 @@
+<script>
+    import logo from "$lib/assets/logo.svg";
+</script>
+
+<div class="flex">
+    <div class="flex items-center h-full p-6 gap-2">
+        <img src="{logo}" alt="" width="32" height="32">
+        <span class="text-4xl font-medium">
+            Chirper
+        </span>
+    </div>
+    <div class="flex">
+
+    </div>
+</div>
