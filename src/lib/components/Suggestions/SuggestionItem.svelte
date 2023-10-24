8ac9f084783e113961613ecd5bac9f17056fb544
SunPodder <73415680+SunPodder@users.noreply.github.com>
2023-10-24 12:59:17 +0600

    add `SuggestionItem`

---
+++ src/lib/components/Suggestions/SuggestionItem.svelte
@@ -0,0 +1,27 @@
+<script>
+	import * as Avatar from '$lib/components/ui/avatar';
+
+    export let name = "", username = "", avatar = "";
+</script>
+
+<div class="flex items-center gap-4 p-4">
+	<div>
+		<Avatar.Root>
+			<Avatar.Image src="{avatar}" alt="@shadcn" />
+			<Avatar.Fallback>CN</Avatar.Fallback>
+		</Avatar.Root>
+	</div>
+	<div>
+        <div class="font-bold">
+            {name}
+        </div>
+        <div class="font-thin">
+            {username}
+        </div>
+    </div>
+    <div class="ml-10">
+        <button class="px-4 py-2 bg-violet-500 rounded-lg text-sm">
+            Follow
+        </button>
+    </div>
+</div>
