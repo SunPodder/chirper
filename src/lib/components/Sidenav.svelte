5ef3601716e72989e8eb367509d7e48f77e7756f
SunPodder <73415680+SunPodder@users.noreply.github.com>
2023-10-24 12:19:23 +0600

    add sidenav

---
+++ src/lib/components/Sidenav.svelte
@@ -0,0 +1,38 @@
+<script>
+    import home from "$lib/assets/home.svg";
+    import search from "$lib/assets/search.svg";
+    import activity from "$lib/assets/heart.svg";
+    import create from "$lib/assets/create.svg";
+    import communities from "$lib/assets/community.svg";
+    import profile from "$lib/assets/user.svg";
+
+</script>
+
+<div>
+    <ul class="p-6 flex flex-col gap-12 mt-10 ml-4">
+        <li class="flex gap-4">
+            <img src="{home}" alt="">
+            <span>Home</span>
+        </li>
+        <li class="flex gap-4">
+            <img src="{search}" alt="">
+            <span>Search</span>
+        </li>
+        <li class="flex gap-4">
+            <img src="{activity}" alt="">
+            <span>Activity</span>
+        </li>
+        <li class="flex gap-4">
+            <img src="{create}" alt="">
+            <span>Create Chirp</span>
+        </li>
+        <li class="flex gap-4">
+            <img src="{communities}" alt="">
+            <span>Communities</span>
+        </li>
+        <li class="flex gap-4">
+            <img src="{profile}" alt="">
+            <span>Profile</span>
+        </li>
+    </ul>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Sidenav from '$lib/components/Sidenav.svelte';
+import Topbar from '$lib/components/Topbar.svelte';
+	import '../app.postcss';
+</script>
+
+<div class="h-screen w-screen ">
+	<Topbar />
+	<div class="flex">
+        <Sidenav />
+        <slot />
+    </div>
+</div>
