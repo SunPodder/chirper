f36f9940cde08c19a73217219f06166c17437c36
SunPodder <73415680+SunPodder@users.noreply.github.com>
2023-10-26 20:46:11 +0600

    add Chirp Component

---
+++ src/lib/components/Chirp/Chirp.svelte
@@ -0,0 +1,58 @@
+<script>
+	import * as Avatar from '$lib/components/ui/avatar';
+	import heart from '$lib/assets/heart-gray.svg';
+	import comment from '$lib/assets/reply.svg';
+	import share from '$lib/assets/share.svg';
+	import repost from '$lib/assets/repost.svg';
+
+	// @type {import("Chirp")}
+	export let chirp = {};
+</script>
+
+<div class="flex">
+	<div class="flex flex-col items-center">
+		<Avatar.Root class="w-10 h-10">
+			<Avatar.Image src={chirp.user.avatar} alt={chirp.user.username} />
+			<Avatar.Fallback>{chirp.user.username}</Avatar.Fallback>
+		</Avatar.Root>
+		<div class="flex-grow w-[2px] bg-gray-700" />
+	</div>
+	<div class="ml-4">
+		<div class="font-bold">
+			{chirp.user.name}
+		</div>
+		<div class="mt-2">
+			{chirp.content}
+		</div>
+		<div class="mt-4 flex gap-3">
+			<button>
+				<img src={heart} alt="" />
+			</button>
+			<button>
+				<img src={comment} alt="" />
+			</button>
+			<button>
+				<img src={share} alt="" />
+			</button>
+			<button>
+				<img src={repost} alt="" />
+			</button>
+		</div>
+	</div>
+</div>
+
+<div class="flex mt-2">
+	<div class="w-10 flex items-center justify-center ml-[4px]">
+		{#each chirp.comments as reply}
+			{#if reply.user}
+				<Avatar.Root class="w-6 h-6 ml-[-6px]">
+					<Avatar.Image src={reply.user.avatar} alt={reply.user.username} />
+					<Avatar.Fallback>{reply.user.username}</Avatar.Fallback>
+				</Avatar.Root>
+			{/if}
+		{/each}
+	</div>
+    <div class="text-xs flex items-center text-gray-500 ml-4">
+        {chirp.comments.length} replies
+    </div>
+</div>
---
+++ src/lib/components/Chirp/ChirpBlock.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Chirp from './Chirp.svelte';
+    export let chirp = {};
+</script>
+
+<div>
+    <Chirp chirp={chirp} />
+</div>
