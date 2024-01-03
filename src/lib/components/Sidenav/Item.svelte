fd06826af2add4403c074ff741ac72aa349da3f4
SunPodder <73415680+SunPodder@users.noreply.github.com>
2024-01-03 14:42:24 +0600

    Update Sidenav

---
+++ src/lib/components/Sidenav/Item.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+
+	export let icon = '';
+	export let href = '';
+</script>
+
+<a {href}>
+	<li class="px-3 py-4 flex gap-4 rounded-lg cursor-pointer" class:active={$page.route.id == href}>
+		<img src={icon} alt="" />
+		<span><slot /></span>
+	</li>
+</a>
+
+<style>
+	.active {
+		@apply bg-purple-500;
+	}
+</style>
---
+++ src/lib/components/Sidenav/Sidenav.svelte
@@ -0,0 +1,21 @@
+<script>
+	import home from '$lib/assets/home.svg';
+	import search from '$lib/assets/search.svg';
+	import activity from '$lib/assets/heart.svg';
+	import create from '$lib/assets/create.svg';
+	import communities from '$lib/assets/community.svg';
+	import profile from '$lib/assets/user.svg';
+	import Item from './Item.svelte';
+
+	let className = '';
+	export { className as class };
+</script>
+
+<ul class="p-6 flex flex-col gap-6 {className}">
+	<Item href="/" icon={home}>Home</Item>
+	<Item href="/search" icon={search}>Search</Item>
+	<Item href="/activity" icon={activity}>Activity</Item>
+	<Item href="/create" icon={create}>Create Chirp</Item>
+	<Item href="/communities" icon={communities}>Communities</Item>
+	<Item href="/profile" icon={profile}>Profile</Item>
+</ul>
