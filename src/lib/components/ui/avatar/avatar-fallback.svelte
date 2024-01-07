6abf067deb70835125cb50ad462315de116bbea9
SunPodder <73415680+SunPodder@users.noreply.github.com>
2024-01-07 21:25:47 +0600

    minor changes

--- src/lib/components/ui/avatar/avatar-fallback.svelte
+++ src/lib/components/ui/avatar/avatar-fallback.svelte
@@ -10,7 +10,7 @@
 
 <AvatarPrimitive.Fallback
 	class={cn(
-		"flex h-full w-full items-center justify-center rounded-full bg-muted",
+		"flex h-full w-full items-center justify-center rounded-full bg-black text-center",
 		className
 	)}
 	{...$$restProps}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 <div class="h-screen w-screen">
 	<Topbar />
 	<div class="sm:flex">
-		<Sidenav class="w-1/6" />
+		<Sidenav class="w-full sm:w-1/2 md:w-1/3 lg:w-1/6" />
 		<slot />
 	</div>
 </div>
