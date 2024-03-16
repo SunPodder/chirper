be5a9748d89d52157a9c043b00efd720178dcd49
SunPodder <contact.sunpodder09@gmail.com>
2024-03-16 13:29:44 +0600

    Add logout functionality and signup page

---
+++ src/routes/signup/+page.server.js
@@ -0,0 +1,45 @@
+import { fail, redirect } from '@sveltejs/kit';
+
+function generateId() {
+	return Math.random().toString(36).substring(2);
+}
+
+export const actions = {
+	default: async function ({ locals, request, cookies }) {
+		let data = Object.fromEntries(await request.formData());
+		let errors = {};
+		data.username = data.username.trim();
+		data.password = data.password.trim();
+
+		data.username ? null : (errors.username = 'Username is required');
+		data.password ? null : (errors.password = 'Password is required');
+
+		if (Object.keys(errors).length) {
+			return fail(400, { error: errors });
+		}
+		
+
+		data = {
+			...data,
+			name: data.username,
+			avatar: `https://api.dicebear.com/7.x/lorelei/jpg?seed=${data.username}`,
+			cover: `https://picsum.photos/seed/${data.username}/800/300`,
+			bio: '',
+			followers: 0,
+			following: 0,
+			posts: 0,
+			address: ''
+		};
+		let sessionId = generateId();
+
+		try {
+			await locals.db.create(`user:${data.username}`, data)[0];
+			await locals.db.create(`session:${sessionId}`, { user: 'user:' + data.username });
+		} catch (e) {
+			return { error: e.message };
+		}
+
+		cookies.set('session', sessionId);
+		throw redirect(303, '/');
+	}
+};
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { enhance } from '$app/forms';
+	import logo from '$lib/assets/logo.svg';
+
+	export let form;
+	$: console.log(form);
+</script>
+
+<div class="w-full mt-32 flex flex-col items-center justify-center gap-4">
+	<img src={logo} alt="" width="140" />
+	<h1 class="font-bold text-3xl text-center">Create an Account</h1>
+	<form method="POST" use:enhance class="flex flex-col gap-2">
+		<div>Enter your email below to create your account</div>
+		{#if form?.error.username}
+			<div class="text-red-500">{form.error.username}</div>
+		{/if}
+		<input
+			name="username"
+			type="text"
+			placeholder="Username"
+			class="w-full rounded-lg p-2 border-gray border-[1px] bg-transparent focus:outline-none"
+		/>
+		{#if form?.error.password}
+			<div class="text-red-500">{form.error.password}</div>
+		{/if}
+		<input
+			name="password"
+			type="password"
+			placeholder="Password"
+			class="w-full rounded-lg p-2 border-gray border-[1px] bg-transparent focus:outline-none"
+		/>
+		<button class="w-full bg-gray-100 text-black rounded-lg p-2"> Sign in with Email </button>
+	</form>
+	<!-- 
+	<div class="text-center">Or continue with</div>
+	<div class="">
+		<button class="rounded-lg p-2 border-gray border-[1px] flex justify-center w-[280px]">
+			<Icon icon="akar-icons:github-fill" class="w-5 h-5" />
+			Github
+		</button>
+	</div> -->
+	<div>By clicking continue, you agree to our Terms of Service and Privacy Policy .</div>
+</div>
